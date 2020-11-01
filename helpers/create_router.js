5e159eb5203ae2efa48de08c27c616580de6994b
emmikoivisto <emetsi@gmail.com>
2020-11-01 14:19:25 +0000

    DELETE route added

--- helpers/create_router.js
+++ helpers/create_router.js
@@ -46,4 +46,18 @@ const createRouter = function (collection) {
           });
       });
 
+    //DELETE
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+        collection
+        .deleteOne({_id: ObjectID(id)})
+        .then(result => {
+            res.json(result)
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err });
+          });
+    });
 }
